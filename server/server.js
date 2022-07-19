53d2695cd9751239632b3f0afc5918f73d9f4634
tmenture <thomasoxemail@gmail.com>
2022-07-19 10:59:16 -0400

    add reaction, add friend, and add thought resolvers added

--- server/server.js
+++ server/server.js
@@ -4,14 +4,17 @@ const { ApolloServer } = require('apollo-server-express');
 
 // Import the typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 // Create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,5 +8,33 @@ module.exports = {
         const payload = { username, email, _id};
 
         return jwt.sign({ data: payload }, secret, {expiresIn: expiration });
+    },
+    authMiddleware: function({ req }) {
+        // Allows token to be sent via req.body, req.query, or headers
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        // Seperates Bearer from <tokenvalue>
+        if (req.headers.authorization) {
+            token = token 
+                .split(' ')
+                .pop()
+                .trim();
+        }
+
+        // If no token, return request object as is
+        if (!token) {
+            return req;
+        }
+
+        try {
+            // Decode and attach user data to request object
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid Token');
+        }
+
+        // Returns updated request object
+        return req;
     }
 };
