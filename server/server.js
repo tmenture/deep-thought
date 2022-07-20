28924df4d97bd8468554247b8e94bdc33ea20f7e
tmenture <thomasoxemail@gmail.com>
2022-07-20 14:04:05 -0400

    hopefully fixed some of the issues i was having

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const path = require('path');
 const express = require('express');
-// Import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-// Import the typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
-// Create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 const app = express();
@@ -21,29 +18,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
-  // Integrate the Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
-
-  // Server up static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
 
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      // Log where we can go to tet our GQL API
-      console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
 };
 
-// Call async function to start the server
+// Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,40 +1,37 @@
 const jwt = require('jsonwebtoken');
 
-const secret = "mysecretsshhhhh";
+const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-    signToken: function({ username, email, _id }) {
-        const payload = { username, email, _id};
-
-        return jwt.sign({ data: payload }, secret, {expiresIn: expiration });
-    },
-    authMiddleware: function({ req }) {
-        // Allows token to be sent via req.body, req.query, or headers
+    authMiddleware: function ({ req }) {
+        // allows token to be sent via req.body, req.query, or headers
         let token = req.body.token || req.query.token || req.headers.authorization;
 
-        // Seperates Bearer from <tokenvalue>
+        // ["Bearer", "<tokenvalue>"]
         if (req.headers.authorization) {
-            token = token 
+            token = token
                 .split(' ')
                 .pop()
                 .trim();
         }
 
-        // If no token, return request object as is
         if (!token) {
             return req;
         }
 
         try {
-            // Decode and attach user data to request object
             const { data } = jwt.verify(token, secret, { maxAge: expiration });
             req.user = data;
         } catch {
-            console.log('Invalid Token');
+            console.log('Invalid token');
         }
 
-        // Returns updated request object
         return req;
+    },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     }
 };
